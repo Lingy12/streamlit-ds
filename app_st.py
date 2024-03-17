865e4e40e72220bed8963551b8ffe1cb6684e142
lgy_workstation <you@example.com>
2024-03-17 23:37:20 +0800

    change download data

--- app_st.py
+++ app_st.py
@@ -63,7 +63,7 @@ if uploaded_file is not None:
         output_df, output_file, err = process_excel_file(tmp_file_path, euro_countries)
         st.header("Output_data")
         st.dataframe(output_df)
-        st.download_button(label="Download Output Excel", data=output_df.to_excel(index=False), file_name=output_file, mime='application/ms-excel')
+        st.download_button(label="Download Output Excel", data=output_df.to_excel(index=False), file_name=output_file)
         if err:
             st.header('Entry error log')
             st.dataframe(pd.DataFrame(err, columns=['Country', 'Year', 'Error']))
